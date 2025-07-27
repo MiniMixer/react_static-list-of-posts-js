8005469c96b62faec7e33d9eab16021164e48b2c
Radomur Vlasuk <radical123boss@gmail.com>
2025-07-27 21:42:05 +0300

    Commit message

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,11 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ post, comments }) => (
+  <div className="CommentList">
+    {comments.map(comment =>
+      comment.postId === post ? (
+        <CommentInfo comment={comment} key={comment.id} />
+      ) : null,)}
+  </div>
+);
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,31 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+function hasPostId(data, targetPostId) {
+  return data.some(item => item.postId === targetPostId);
+}
+
+export const PostInfo = ({ post, comments }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <UserInfo user={post.user} />
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
+    {hasPostId(comments, post.id) ? (
+      <CommentList post={post.id} comments={comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
+);
